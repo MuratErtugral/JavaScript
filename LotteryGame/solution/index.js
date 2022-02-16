6da434504a8f5c7a7e82534d0822275310e49c6c
MuratErtugral <muratertugral93@gmail.com>
2022-02-16 11:50:21 +0300

    Starter

--- LotteryGame/solution/index.js
+++ LotteryGame/solution/index.js
@@ -15,8 +15,8 @@ let luckyNumbers = []
 btn.addEventListener("click",() =>{
     let v = number.value
     let z = 0
-    while(z<=v-1){
-    for (i=0;luckyNumbers.length<6;i++){
+    while(z<v){
+    while(luckyNumbers.length<6){
         let control = lucky()
         if (luckyNumbers.includes(control)){continue}
         else {luckyNumbers.push(control)}
@@ -30,17 +30,16 @@ btn.addEventListener("click",() =>{
         else {luckyNumbers.push(control)}
     }
     luckyNumbers.push(lucky())
-    let h = luckyNumbers.join("-")
+    
     let c =`${luckyNumbers[0]}-${luckyNumbers[1]}-${luckyNumbers[2]}-${luckyNumbers[3]}-${luckyNumbers[4]}-${luckyNumbers[5]} | ${luckyNumbers[6]} | ${luckyNumbers[7]}`
     
-    console.log(c);
     cont[z].innerHTML=c
     luckyNumbers=[]
      
     
     z++
     }
-    if (v-1<7){
+    if (v<8){
         d = v
         for (d;d<8;d++)
         cont[d].innerHTML=""
@@ -48,6 +47,3 @@ btn.addEventListener("click",() =>{
         
     }
 })
-
-
-console.log(luckyNumbers);
