ff132219e14bfd14807e79609dfc0a1a08336a91
MuratErtugral <muratertugral93@gmail.com>
2022-02-17 11:40:09 +0300

    V1.05

--- ToDoList/solution/index.js
+++
@@ -1,8 +0,0 @@
-let happy = document.getElementById("happy");
-let day = document.getElementById("day")
-
-
-
-let a = new Date();
-let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-day.innerText =days[a.getDay()]
